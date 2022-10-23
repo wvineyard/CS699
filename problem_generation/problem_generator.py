f3a2850102419949ffa5ed3bd6afa59de7b4faf4
Bill Vineyard <wvineyard@wisc.edu>
2022-10-23 12:42:22 -0500

    problem generator correctly generates an algebraic problem and solution

--- problem_generation/problem_generator.py
+++ problem_generation/problem_generator.py
@@ -1,5 +1,9 @@
 import random
+from io import StringIO
+from contextlib import redirect_stdout
 
+globals_param = {'__builtins__' : None}
+locals_param = {'ans' : ""}
 class generator_helper(): 
     
     def var_operator(var, value=None):
@@ -12,18 +16,29 @@ class generator_helper():
         return f"{var}{op}{value}"
 
 def gen_algorithm(vars: dict): 
+    noVals = False
     out = ""
-    for i, var in enumerate(vars.keys()): 
+    for i, var in enumerate(vars.keys()):
+        if vars[var] is None: 
+            noVals = True
         out += "(" + generator_helper.var_operator(var, vars[var]) + ")"
         if i != len(vars.keys()) - 1:
             out += random.choice(["+", "-"])
         exec(f"{var} = {vars[var]}")
     loc = {}
-    exec(f"{out}", loc)
-    print(ans)
-    return out, ans
+    if not noVals:
+        f = StringIO()
+        with redirect_stdout(f): 
+            exec(f"print({out})")  
+        s = f.getvalue() 
+        print(s)
+        return out, float(s.strip())
+    else: 
+        return out, "Null"    
 
 if __name__ == '__main__':
     vars = {'x': 1, 'y': 2, 'z': 3}
+    vars2 = {'x': None, 'y': None}
     # print(generator_helper.var_operator('x'))
     print(gen_algorithm(vars))  
+    print(gen_algorithm(vars2))  
